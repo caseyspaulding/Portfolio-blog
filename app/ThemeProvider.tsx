adbe6bfc0f96e6a6f592eb5549533ad943fff59a
Casey Spaulding <cspau17@wgu.edu>
2025-01-27 07:35:37 -0500

    updated to defalt to light mode

--- app/ThemeProvider.tsx
+++ app/ThemeProvider.tsx
@@ -8,8 +8,8 @@ export default function ThemeProviders ( { children }: { children: ReactNode } )
   return (
     <ThemeProvider
       attribute="class"       // ensures Next Themes adds `class="dark"` or `class="light"` on <html>
-      defaultTheme="system"   // or "dark" or "light"
-      enableSystem={ true }     // enable OS-based dark/light preference
+      defaultTheme="light"   // or "dark" or "light"
+      enableSystem={ false }     // enable OS-based dark/light preference
     >
       { children }
     </ThemeProvider>
